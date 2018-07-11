7dffc140d480ca23159b92139a8cbea28c963f1b
vatsalMehra <rdbcasillas11@gmail.com>
2018-07-10 22:58:53 -0500

    margin convention

--- js/main.js
+++ js/main.js
@@ -5,5 +5,26 @@
 */
 
 d3.json("data/data.json").then(function(data){
-	console.log(data);
+
+    const w = 800;
+    const h = 800;
+
+    const svg = d3.select("#chart-area")
+                .append("svg")
+                .attr("width", w)
+                .attr("height", h);
+
+    // margin convention
+    const margin = { left: 20, right: 20, top: 20, bottom: 20 };
+
+    const innerWidth = w - margin.left - margin.right
+    const innerHeight = h - margin.top - margin.bottom
+
+    const g = svg.append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+
+                
 })
